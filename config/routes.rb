c8510d06cb1f95660383d28699ce985a8577a827
Naveed <naveed.ar.khan@gmail.com>
2013-12-29 15:33:44 -0800

    Destroyed topics, posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Bloccit::Application.routes.draw do
   devise_for :users
 
   resources :topics do
-    resources :posts, except: [:index]
-      resources :comments, only: [:create]
+    resources :posts, except: [:index] do
+      resources :comments, only: [:create, :destroy]
     end
   end
 
